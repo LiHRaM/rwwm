bcb9644d5eeefa98b8135e1429d7794cd260be5e
Hilmar Gústafsson <hilmargustafs@gmail.com>
2020-07-02 10:14:07 +0200

    Autodetect backend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//! RWWM is an [Anvil](https://github.com/smithay/smithay/) fork which adds tiling window management.
+
 #![warn(rust_2018_idioms)]
 
 #[macro_use]
@@ -9,7 +11,7 @@ extern crate smithay;
 
 use std::{cell::RefCell, rc::Rc};
 
-use slog::Drain;
+use slog::{Drain, Logger};
 use smithay::reexports::{calloop::EventLoop, wayland_server::Display};
 
 #[macro_use]
@@ -28,46 +30,33 @@ mod winit;
 
 use state::AnvilState;
 
-static POSSIBLE_BACKENDS: &[&str] = &[
-    #[cfg(feature = "winit")]
-    "--winit : Run anvil as a X11 or Wayland client using winit.",
-    #[cfg(feature = "udev")]
-    "--tty-udev : Run anvil as a tty udev client (requires root if without logind).",
-];
+static DISPLAY: &str = "DISPLAY";
 
-fn main() {
-    // A logger facility, here we use the terminal here
-    let log = slog::Logger::root(
+fn start_logger() -> Logger {
+    slog::Logger::root(
         slog_async::Async::default(slog_term::term_full().fuse()).fuse(),
         o!(),
-    );
+    )
+}
+
+fn main() {
+    let log = start_logger();
 
     let mut event_loop = EventLoop::<AnvilState>::new().unwrap();
     let display = Rc::new(RefCell::new(Display::new()));
 
-    let arg = ::std::env::args().nth(1);
-    match arg.as_ref().map(|s| &s[..]) {
-        #[cfg(feature = "winit")]
-        Some("--winit") => {
-            info!(log, "Starting anvil with winit backend");
-            if let Err(()) = winit::run_winit(display, &mut event_loop, log.clone()) {
-                crit!(log, "Failed to initialize winit backend.");
-            }
-        }
+    let is_tty = std::env::var_os(DISPLAY).is_none();
+    if is_tty {
+        info!(log, "Starting anvil on a tty using udev");
         #[cfg(feature = "udev")]
-        Some("--tty-udev") => {
-            info!(log, "Starting anvil on a tty using udev");
-            if let Err(()) = udev::run_udev(display, &mut event_loop, log.clone()) {
-                crit!(log, "Failed to initialize tty backend.");
-            }
+        if let Err(()) = udev::run_udev(display, &mut event_loop, log.clone()) {
+            crit!(log, "Failed to initialize tty backend.");
         }
-        _ => {
-            println!("USAGE: anvil --backend");
-            println!();
-            println!("Possible backends are:");
-            for b in POSSIBLE_BACKENDS {
-                println!("\t{}", b);
-            }
+    } else {
+        info!(log, "Starting anvil with winit backend");
+        #[cfg(feature = "winit")]
+        if let Err(()) = winit::run_winit(display, &mut event_loop, log.clone()) {
+            crit!(log, "Failed to initialize winit backend.");
         }
     }
 }
